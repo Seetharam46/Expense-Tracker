907a9313dcc780f57e6e21f1647f7a34b697cdb7
Seetharam46 <seetharam.p2004@gmail.com>
2025-08-04 17:15:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,45 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = 'expense-pwa-v2';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (evt) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open('expense-tracker-v1').then(cache => {
-      return cache.addAll([
-        '/expense-tracker/',
-        '/expense-tracker/index.html',
-        '/expense-tracker/style.css',
-        '/expense-tracker/script.js',
-        '/expense-tracker/manifest.json',
-        '/expense-tracker/icon-192.png',
-        '/expense-tracker/icon-512.png'
-      ]);
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== 'expense-tracker-v1').map(k => caches.delete(k)))
-    )
+
+self.addEventListener('activate', (evt) => {
+  console.log('Service worker activating');
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if(key !== CACHE_NAME){
+            console.log('Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
-  return self.clients.claim();
+  self.clients.claim();
 });
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request))
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
   );
 });
